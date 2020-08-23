d100f3e41bf5c14192562a45e6756fc07563bb70
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-08-23 16:01:55 -0700

    CSS - abstraction, & new Tailwind properties

--- src/assets/cssClasses.ts
+++ src/assets/cssClasses.ts
@@ -1,7 +1,11 @@
 export const AppContainer = 'flex px-0 py-3 lg:p-10 lg:pt-5 bg-indigo-200 bg-opacity-75 lg:justify-around justify-center min-h-screen';
 
-// Buttons
+// Settings buttons
 export const activeBtnState = 'bg-teal-400 text-white rounded-full shadow-lg font-thin px-5 py-2 m-1 border border-solid border-teal-300';
-export const inactiveBtnState = 'border border-solid border-teal-300 text-teal-400 bg-white rounded-full shadow-lg font-thin px-5 py-2 m-1'; 
-export const activeBtnStateSm = 'bg-teal-400 text-white h-8 w-24 rounded-full shadow-lg font-thin mr-1';
-export const inactiveBtnStateSm = 'border border-solid border-teal-300 text-teal-400 bg-white h-8 rounded-full shadow-lg mr-1 w-24 font-light'; 
+export const inactiveBtnState = 'border border-solid border-teal-300 text-teal-400 bg-white rounded-full shadow-lg font-thin px-5 py-2 m-1';
+export const activeBtnStateSm = 'bg-teal-400 text-white h-8 w-12 rounded-full shadow-lg font-thin mr-1';
+export const inactiveBtnStateSm = 'border border-solid border-teal-300 text-teal-400 bg-white h-8 rounded-full shadow-lg mr-1 w-12 font-light';
+
+// Modal buttons
+export const activeModalBtn = `bg-teal-600 py-2 w-12 rounded-lg shadow-lg text-white border border-solid border-teal-400`;
+export const inactiveModalBtn = `bg-teal-500 py-2 w-12 rounded-lg shadow-lg text-white`;

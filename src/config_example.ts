a1777007a6b6ee47107024ef233f47e82f30aafb
Ryan Knuesel <ryan@mypaynet.com>
2020-08-26 08:49:29 -0500

    Update to use environment variables
    This allows me to build the script using automated actions where creating config files is not really possible.

--- src/config_example.ts
+++
@@ -1,9 +0,0 @@
-export const host = '';
-export const headers = {};
-const port = 3000;
-
-const city = 'Madison';
-const state = 'Wisconsin';
-export const timeZone = 'America/Chicago';
-
-export default { host, port, city, state, headers };

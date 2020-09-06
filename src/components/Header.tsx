f524d61b7f65eca4f1ac4dc259507f10050b18e7
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-09-06 13:55:24 -0700

    Create Mobile-Friendly Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,36 +7,44 @@ import { ReactComponent as MenuSvg } from '../assets/svgs/menu.svg';
 
 function Header({ headerData, date, settings, toggleRaspberrySettings }: HeaderProps) {
 	const { degreesFormat, use24Hour } = settings;
-
 	const dayOfWeek = Calendar.daysOfWeek[date.getDay() - 1];
 	const dateStr = formatDateStr(date);
 	const temperature = scrubTemperature(headerData.temperature, degreesFormat);
 	const time = formatTime(use24Hour, date);
+
+	/*
+		'': default, mobile styling
+		sm: RaspberryPI UI styling
+		lg: desktop styling
+
+		Note: sm declarations overwrite mobile styles, lg overwrites sm and and mobile styles
+	*/
 	return (
-		<div className="flex flex-row bg-white py-2 px-12 sm:px-6 sm:py-1 shadow-xl rounded-lg">
-			<div className="flex flex-row w-7/12 mx-auto justify-start items-center">
-				<div className="flex flex-col font-light lg:text-3xl text-2xl text-left text-blue-300">
-					<div className="flex-row">
-						<span className="mr-1">
-							{`${headerData.city},`}
-						</span>
-						<span className="mr-5">
-							{headerData.state}
-						</span>
+		<div className="flex flex-col shadow-xl rounded-lg sm:flex-row bg-white px-3 py-2 sm:px-6 sm:py-1 lg:py-2 lg:px-12">
+			<div className="flex flex-row justify-between items-center lg:w-7/12 lg:mx-auto ">
+				<div className="flex items-center">
+					<div className="flex font-light text-left text-blue-300 text-lg sm:text-2xl lg:text-3xl">
+						<div className="flex-row">
+							<span className="mr-1">
+								{`${headerData.city},`}
+							</span>
+							<span className="mr-2 sm:mr-5">
+								{headerData.state}
+							</span>
+						</div>
+					</div>
+					<div className="mr-2 sm:mr-5">
+						<div className="flex font-thin text-3xl text-teal-300 items-start sm:text-5xl lg:text-6xl">{`${temperature}\u00b0${degreesFormat[0]}`}</div>
 					</div>
 				</div>
-				<div className="mr-5">
-					<div className="flex font-thin lg:text-6xl text-5xl text-teal-300 items-start">{`${temperature}\u00b0${degreesFormat[0]}`}</div>
-				</div>
 				<button className="inline-flex lg:hidden" onClick={() => toggleRaspberrySettings()}>
-					<MenuSvg className="w-8 h-8" />
+					<MenuSvg className="w-6 h-6 sm:w-8 sm:h-8" />
 				</button>
 			</div>
-
-			<div className="w-5/12 flex flex-col text-right">
-				<div className="font-thin lg:text-5xl text-4xl mb-2 sm:mb-0 text-teal-300">{time}</div>
-				<div className="font-thin lg:text-2xl text-xl text-gray-500">{dayOfWeek}</div>
-				<div className="font-thin lg:text-2xl text-xl text-gray-500">{dateStr}</div>
+			<div className="justify-end items-center flex flex-row-reverse sm:w-5/12 sm:flex-col sm:items-end">
+				<div className="font-thin text-2xl mb-0 text-teal-300 text-left sm:text-right sm:mr-0 sm:text-4xl lg:text-5xl lg:mb-2">{time}</div>
+				<div className="font-thin text-lg text-gray-500 lg:text-2xl">{dayOfWeek}</div>
+				<div className="font-thin text-lg text-gray-500 mr-2 sm:m-0 lg:text-2xl">{dateStr}</div>
 			</div>
 		</div>
 	);

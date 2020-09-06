5938b0c337b51314a5c2776e13f1783f2f5b6fd3
Jorge Castello <60675751+jorgio-castello@users.noreply.github.com>
2020-09-06 15:05:19 -0700

    Create Mobile Friendly Settings Panel

--- src/components/Settings/RaspberrySettings.tsx
+++ src/components/Settings/RaspberrySettings.tsx
@@ -5,12 +5,12 @@ import SettingsButton from '../UserInterface/SettingsButton';
 export default function ({ settings, setSettings }: RaspberrySettingsProps) {
 	const { degreesFormat, use24Hour, thermInterval } = settings;
 	return (
-		<div className="flex h-40 bg-white bg-opacity-50 mt-3 rounded-lg">
-			<div className="w-1/2 flex flex-col h-full justify-start pl-5 pt-5">
+		<div className="flex flex-col h-auto bg-white bg-opacity-50 mt-3 rounded-lg sm:flex-row ">
+			<div className="w-full flex flex-col h-full justify-start pl-5 pt-5 sm:w-1/2">
 				<div className="text-left text-3xl font-bold text-blue-600 tracking-tight">{`Therm\u00b0`}</div>
 				<input className="h-10 rounded-lg border border-solid border-gray-300 w-10/12 pl-2 font-thin shadow-lg" type="text" placeholder="Enter a city" />
 			</div>
-			<div className="w-1/2 flex flex-col justify-around py-2 pr-5">
+			<div className="w-full flex flex-col justify-around pl-6 pt-3 pb-5 sm:py-2 sm:pr-5 sm:w-1/2">
 				<div className="flex flex-row items-center justify-between mt-1">
 					<div className="w-1/12 font-light text-blue-500 text-sm mr-2">Temperature:</div>
 					<div className="w-8/12">
@@ -24,17 +24,17 @@ export default function ({ settings, setSettings }: RaspberrySettingsProps) {
 						))}
 					</div>
 				</div>
-				<div className="flex flex-row mt-1 items-center justify-between flex-wrap">
+				<div className="flex flex-row my-3 items-center justify-between flex-wrap">
 					<div className="w-1/12 font-light text-blue-500 text-sm mr-2">Time:</div>
 					<div className="w-8/12 justify-start">
 						<SettingsButton active={use24Hour} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'use24Hour', true)}>12H</SettingsButton>
 						<SettingsButton active={!use24Hour} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'use24Hour', false)}>24H</SettingsButton>
 					</div>
 				</div>
-				<div className="flex flex-row mt-1 items-center flex-wrap justify-between">
+				<div className="flex flex-row items-center flex-wrap justify-between">
 					<div className="w-1/12 font-light text-blue-500 text-sm mr-2">Interval:</div>
-					<div className="w-auto justify-start">
-						<SettingsButton active={thermInterval === 289} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'thermInterval', 289)}>24H</SettingsButton>
+					<div className="w-8/12 sm:w-auto justify-start">
+						<SettingsButton active={thermInterval === 289} size=" h-8 w-12 mr-1 mb-1" onClick={() => setSettings(settings, 'thermInterval', 289)}>24H</SettingsButton>
 						<SettingsButton active={thermInterval === 145} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'thermInterval', 145)}>12H</SettingsButton>
 						<SettingsButton active={thermInterval === 73} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'thermInterval', 73)}>6H</SettingsButton>
 						<SettingsButton active={thermInterval === 13} size=" h-8 w-12 mr-1" onClick={() => setSettings(settings, 'thermInterval', 13)}>1H</SettingsButton>
